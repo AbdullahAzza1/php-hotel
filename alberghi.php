859eb38c254c90240e580f3a19ccb6fc1971d174
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-12-18 17:18:40 +0100

    step1

--- alberghi.php
+++ alberghi.php
@@ -40,4 +40,21 @@ $hotels = [
 
 ];
 
+foreach ($hotels as $albergo) {
+    echo $albergo;
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div> <?php $albergo ?></div>
+</body>
+</html>
+
